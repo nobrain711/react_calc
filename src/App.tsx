9d1f1c26dc13db864db8e13d59b5100c822af622
nobrain711 <qa9975@ync.ac.kr>
2024-06-11 10:34:09 +0900

    init: crate project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Typography from '@mui/material/Typography'
+
+function App() {
+
+  return (
+    <Typography className='flex flex-auto text-ellipsis text-3xl'> hello world</Typography>
+  )
+}
+
+export default App
