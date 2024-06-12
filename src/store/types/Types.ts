ebf8d515a33ccae7c8acfa655de2ef7a8a3884e3
nobrain711 <qa9975@ync.ac.kr>
2024-06-12 17:45:36 +0900

    WIP: redux creating

--- src/store/types/Types.ts
+++ src/store/types/Types.ts
@@ -4,15 +4,3 @@ export const CALC_PROCESS = "CALC_PROCESS";
 export const CLEAR = "CLAER";
 export const CLEAR_ENTER = "CLEAR_ENTER";
 export const BACK_SPACE = "BACK_SPACE";
-
-export interface result {
-  resultNumber: number;
-  exprssion: string;
-  display: boolean;
-}
-
-export interface calc {
-  number: number;
-  operation: string;
-  result: result;
-}
